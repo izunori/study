67434dc9888f8f75960def50dd62632430e96a9d
izunori <izuharakun53@yahoo.co.jp>
2019-08-04 09:38:26 +0900

    feat : add second implementation of binary tree

--- binary_tree.py
+++ binary_tree.py
@@ -67,7 +67,59 @@ class BinTree:
 	def delete(self,val):
 		self.val = self.val.delete(val)
 
-bt = BinTree()
+class BinTree2:
+	def __init__(self,val = None):
+		self.val = val
+		if not self.val is None:
+			self.left = BinTree2()
+			self.right = BinTree2()
+	def judge(self,val):
+		if val <= self.val:
+			return self.left
+		else:
+			return self.right
+	def add(self,val):
+		if self.val is None:
+			self.__init__(val)
+			return 
+		self.judge(val).add(val)
+	def get(self):
+		if self.val:
+			return self.left.get()+[self.val] + self.right.get()
+		else:
+			return []
+	def find(self,val):
+		if self.val is None:
+			return False
+		if self.val == val:
+			return True
+		return self.judge(val).find(val)
+	def transform(self,node):
+		self.val = node.val
+		if self.val is None:
+			del self.left
+			del self.right
+		else:
+			self.left = node.left
+			self.right = node.right
+	def min(self):
+		return self.left.min() if self.left.val else self.val
+	def max(self):
+		return self.right.max() if self.right.val else self.val
+	def delete(self,val):
+		if self.val == val:
+			if self.left.val is None:
+				self.transform(self.right)
+			elif self.right.val is None:
+				self.transform(self.left)
+			else:
+				self.val = self.left.max()
+				self.left.delete(self.val)
+			return
+		self.judge(val).delete(val)
+
+
+bt = BinTree2()
 bt.add(5)
 bt.add(3)
 bt.add(7)
@@ -75,7 +127,9 @@ bt.add(10)
 bt.add(6)
 bt.add(4)
 print(bt.get())
+print(bt.find(10))
 print(bt.find(2))
-bt.delete(7)
+bt.delete(3)
 bt.delete(6)
 print(bt.get())
+print(bt.find(6))
