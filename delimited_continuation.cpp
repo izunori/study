8bc06ddf4e9f8e4df2c1f790e08d2fd571400bd0
izunori <izuharakun53@yahoo.co.jp>
2022-12-17 21:49:45 +0900

    not work save

--- delimited_continuation.cpp
+++ delimited_continuation.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 #include<tuple>
 #include<vector>
+#include<string>
+
+
+void print() {std::cout << std::endl;}
+
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail)
+{
+  std::cout << head << ",";
+  print(std::forward<Tail>(tail)...);
+}
 
 template<typename A, typename B>
 using Hom = std::function<B(A)>;
@@ -63,17 +74,29 @@ Cont<int,int> fact(int n){
 }
 
 template<typename T, typename T2, typename R>
-//Cont<T,R> call_cc(Hom<Hom<T,Cont<T2,R>>,Cont<T,R>> f){
-Cont<T,R> call_cc(Hom<KHom<T,T2,R>,Cont<T,R>> f){
+Cont<T,R> call_cc(Hom<Hom<T,Cont<T2,R>>,Cont<T,R>> f){
+//Cont<T,R> call_cc(Hom<KHom<T,T2,R>,Cont<T,R>> f){
     return [f](Hom<T,R> k) -> R{
-        //Hom<T, Cont<T2,R>> g = [&k](T t){
-        KHom<T,T2,R> g = [k](T t){
+        Hom<T, Cont<T2,R>> g = [&k](T t){
+        //KHom<T,T2,R> g = [k](T t){
             return [k,t](Hom<T2,R> x){
                 return k(t);
             };
         };
         return f(g)(k);
     };
+}
+
+template<typename T2, typename R>
+Cont<int,R> prod2(std::vector<int>& v, int i){
+    return call_cc<int,T2,R>([i,&v](Hom<int,Cont<T2,R>> cc) -> Cont<int,R>{
+            print(i,v[i]);
+            if(i == v.size()) return eta<int,R>(1);
+            //if(v[i] == 0) return cc(0);
+            if(v[i] == 0) return [cc](Hom<int,R> f){return cc(0)([](T2 x){return std::vector<int>();});}; // Rですらある必要がないのはなぜ？
+            Hom<int,int> ele = [&v,i](int x){return x * v[i];};
+            return contF<int,int,R>(ele)(prod2<T2,R>(v,i+1));
+        });
 }
 
 template<typename T, typename T2, typename R>
@@ -83,16 +106,45 @@ KHom<T,T2,R> toKleisli(Hom<T,T2> f){
     };
 }
 
+Hom<int, int> mul(int n){
+    return [n](int x){return n * x;};
+}
+
 template<typename R>
 Cont<int,R> prod(std::vector<int>& v, int i){
     return call_cc<int,int,R>([i,&v](KHom<int,int,R> cc) -> Cont<int,R>{
-                std::cout << i << "," << v[i] << std::endl;
-                if(i == v.size()) return eta<int,R>(1);
-                if(v[i] == 0) return cc(0);
-                Hom<int,int> ele = [&v,i](int x){return x * v[i];};
-                return contF<int,int,R>(ele)(prod<R>(v,i+1));
-            });
+            print(i,v[i]);
+            if(i == v.size()) return eta<int,R>(1);
+            if(v[i] == 0) {print("find 0");return cc(0);};
+            //Hom<int,int> ele = [&v,i](int x){print("mul",x);return x * v[i];};
+            //return contF<int,int,R>(ele)(prod<R>(v,i+1));
+            return contF<int,int,R>(mul(v[i]))(prod<R>(v,i+1));
+        });
 }
+
+//template<typename R>
+//Hom<int,Cont<int,R>> save;
+
+template<typename R>
+class Fact{
+public:
+    Hom<int,Cont<int,R>> save;
+    
+    Cont<int,R> fact(int n){
+    print("fact", n);
+    if(n == 1){
+        return call_cc<int,int,R>([this](Hom<int,Cont<int,R>> cc)->Cont<int,R>{
+                this->save = cc;
+                //this->save = [](int x){return eta<int,R>(10*x);};
+                std::cout << &(cc) << std::endl;
+                std::cout << &(this->save) << std::endl;
+                return eta<int,R>(1);
+                });
+    } else {
+        return contF<int,int,R>(mul(n))(fact(n-1));
+    };
+}
+};
 
 int main(){
 
@@ -102,15 +154,22 @@ int main(){
     std::cout << eta<int,int>(5)(twice) << std::endl;
     std::cout << fact(10)(id) << std::endl;
 
-    Hom<int, int> f = fact2;
-    Cont<Hom<int,int>,int> cf = eta<Hom<int,int>, int>(f);
-    Hom<int, Cont<int,int>> cf2 = mac(cf);
-    std::cout << cf2(10)(id) << std::endl;
+    //Hom<int, int> f = fact2;
+    //Cont<Hom<int,int>,int> cf = eta<Hom<int,int>, int>(f);
+    //Hom<int, Cont<int,int>> cf2 = mac(cf);
+    //std::cout << cf2(10)(id) << std::endl;
 
-    std::vector<int> v = {1,2,0,4,5};
+    std::vector<int> v = {1,2,3,0,5};
     Cont<int,int> c = prod<int>(v,0);
     std::cout << c(id) << std::endl;
-    Cont<int,void> c2 = prod<void>(v,0);
-    c2(print);
+    //Cont<int,void> c2 = prod<void>(v,0);
+    //c2(print);
+    Cont<int,void> c3 = prod2<std::string, void>(v,0);
+    c3(print);
+
+    Fact<int> fact;
+    print(fact.fact(3)(id));
+    std::cout << &(fact.save) << std::endl;
+    std::cout << fact.save(2)(id) << std::endl;
     return 0;
 }
