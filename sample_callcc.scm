afb79d4520e8311598e5029e1c2b00ea6df8cdac
izunori <izuharakun53@yahoo.co.jp>
2022-12-18 18:45:39 +0900

    Add another example

--- sample_callcc.scm
+++ sample_callcc.scm
@@ -1,15 +1,16 @@
 ; Reference: https://www.shido.info/lisp/callcc.html
-(define mul (lambda (x y) (begin (display "mul,")(display (* x y))(newline)(* x y))))
+(define mul (lambda (x y) (begin (display "mul,")(print (* x y))(* x y))))
+(define print (lambda x (begin (display x)(newline))))
 (define prod
   (lambda (ls)
     (call/cc
      (lambda (cc)
-       (begin (display ls) (newline)
+       (begin (print ls)
        (if (null? ls)
            1
          (let ((x (car ls)))
            (if (= 0 x)
-               (begin (display "find 0")(newline)(cc 0))                           ;(2)-a
+               (begin (print "find 0")(cc 0))                           ;(2)-a
              (mul x (prod (cdr ls)))
            )
          )
@@ -17,5 +18,28 @@
        )
        )))  
   )
+
+; Reference: http://www.stdio.h.kyoto-u.ac.jp/jugyo1/scheme/SchemeNotes/continuation.html
+(define (mul-cont x)
+	  (call/cc (lambda (escape)
+	     (letrec ((mul-aux (lambda (y) (print y)
+                ;-----------
+				(let ((result
+    				       (cond ((null? y) 1)
+				             ((= (car y) 0) (begin (print "find 0")(escape 0)))
+					     (#t (mul (car y) (mul-aux (cdr y))))
+                         )))
+			           (print (list y result))
+				   result)
+                ;-----------
+				 )))
+	       (mul-aux x) ; 結局これが返る
+           )
+         ))
+      )
+
 (define v '(1 2 3 0 5))
-(display (+ 100 (prod v)))
+(print (prod v))
+(print (+ 100 (prod v)))
+(print "# second")
+(print (mul-cont v))
